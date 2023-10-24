3ead0f7af2624026d94b76133d2a7b18dddf8b5c
DOGA EGE INHANLI <DINHANLI18@ku.edu.tr>
2023-10-24 21:40:11 +0300

    huber_loss_naive corrected

--- comp411_assignment1_2023_questions/comp411/classifiers/linear_svm.py
+++ comp411_assignment1_2023_questions/comp411/classifiers/linear_svm.py
@@ -96,23 +96,29 @@ def huber_loss_naive(W, X, y, reg):
     ###############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     
-    # compute the loss and the gradient
+   # compute the loss and the gradient
     num_classes = W.shape[1]
     num_train = X.shape[0]
     loss = 0.0
-    delta = 0.5
+    
     for i in range(num_train):
         scores = X[i].dot(W)
         correct_class_score = scores[y[i]]
         for j in range(num_classes):
             if j == y[i]:
                 continue
-            margin = scores[j] - correct_class_score + delta # modified the delta
-            if margin > 0:
-                loss += margin
-                dW[:, j] += X[i]
-                dW[:, y[i]] -= X[i]
-
+            margin = scores[j] - correct_class_score
+            
+            if margin > -1:
+                if margin > 1:
+                    loss += 4*margin
+                    dW[:, j] += 4*X[i]
+                    dW[:, y[i]] -= 4*X[i]
+                else:
+                    loss += (1+margin)**2
+                    dW[:, j] += 2*(margin+1)*X[i]
+                    dW[:, y[i]] -= 2*(margin+1)*X[i]
+            
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
     loss /= num_train
